601d227be533c297ac14d2d065be9577c9fc6c6c
ryigitkader <ryigitkader@gmail.com>
2021-12-24 17:32:29 +0300

    initial

---
+++ src/main/java/com/robotmq/client/RobotMqClientSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.robotmq.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotMqClientSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RobotMqClientSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/robotmq/client/RobotMqClientSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robotmq.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotMqClientSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
