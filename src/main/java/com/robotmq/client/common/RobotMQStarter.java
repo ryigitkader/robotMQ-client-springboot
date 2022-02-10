def4d6119b51c21581c663544e44ae01fa686160
ryigitkader <ryigitkader@gmail.com>
2022-02-10 19:42:51 +0300

    test

--- src/main/java/com/robotmq/client/common/RobotMQStarter.java
+++ src/main/java/com/robotmq/client/common/RobotMQStarter.java
@@ -40,6 +40,7 @@ public class RobotMQStarter {
     @Scheduled(fixedRate = 1000)
     private void timingProduce() throws InterruptedException, JsonProcessingException {
         RobotMQProducer producer = RobotMQProducer.getINSTANCE();
+        producer.produce("topic1", UUID.randomUUID().toString());
         producer.produce("topic2", UUID.randomUUID().toString());
         producer.produce("topic3", p);
     }
